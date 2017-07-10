596534ffe429dbdc46eabbc715b6aca7c59687d7
Lee Butler <butler1233@hotmail.com>
2017-07-10 17:24:38 +0100

    Added ReplenishmentTypes

---
+++ WhiteHingePCL/Classes/ReplenishmentTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteHingePCL.Classes
+{
+    public enum ReplenishmentTypes
+    {
+        Issues = 1,
+        Demand = 2,
+        Low_Levels = 3
+    }
+}
