47783bae962242083b3c0a7ea0b7f0e6f2140772
Lee Butler <butler1233@hotmail.com>
2017-07-17 10:57:19 +0100

    Added Newtonsoft and set Json Constructor Zonedata
    If we have multiple constructors for a class, apparently JSON.NET can't
    work out which one to use unless it has an attribute.

--- WhiteHingePCL/Classes/ZoneData.cs
+++ WhiteHingePCL/Classes/ZoneData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -75,6 +76,14 @@ namespace WhiteHingePCL.Classes
             LocationInfo = loc;
             ItemData = item;
             CurrentZoneDataType = ZoneDataType.LowLevel;
+            
+        }
+        /// <summary>
+        /// This constructor is only for use by Newtonsoft
+        /// </summary>
+        [JsonConstructor]
+        public ZoneData()
+        {
             
         }
         /// <summary>
