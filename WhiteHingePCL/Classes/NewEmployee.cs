d8102b0a1fc401c981cfac2cbb8eeb27a588e34a
Owen <Floul1@hotmail.co.uk>
2017-07-17 10:41:21 +0100

    NewEmployees

---
+++ WhiteHingePCL/Classes/NewEmployee.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteHingePCL.Classes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NewEmployee
+    {
+        /// <summary>
+        /// The user's ID, as displayed in the User's card
+        /// </summary>
+        public int UserId;
+        /// <summary>
+        /// User's First Name
+        /// </summary>
+        public string FirstName;
+        /// <summary>
+        /// User's Surname
+        /// </summary>
+        public string Surname;
+        /// <summary>
+        /// The MD5 Hashed pin of the user 
+        /// </summary>
+        public string HashedPin; // Todo Update to a better security algorithm
+        /// <summary>
+        /// Timeout in seconds
+        /// </summary>
+        public int Timeout; //Timeout in seconds
+
+        /// <summary>
+        /// The Active Directory user account for this user
+        /// </summary>
+        public string AdUser;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PayrollId => UserId;
+        /// <summary>
+        /// The user's full name
+        /// </summary>
+        public string Fullname => FirstName + " " + Surname;
+        /// <summary>
+        /// Constructor for use with SelectDataDictionary
+        /// </summary>
+        /// <param name="sqlData"></param>
+        public NewEmployee(Dictionary<string, object> sqlData)
+        {
+            UserId = Convert.ToInt32(sqlData["PayrollNo"]);
+            FirstName = sqlData["FirstName"].ToString();
+            Surname = sqlData["Surname"].ToString();
+            Visible = Convert.ToInt32(sqlData["NotShowOnTable"]) != 1;
+            HashedPin = sqlData["HashedPin"].ToString();
+            AdUser = sqlData["ActiveDirectoryUser"].ToString();
+            try
+            {
+                Timeout = Convert.ToInt32(sqlData["LoginTimeout"]);
+            }
+            catch (Exception )
+            {
+                Timeout = 300;
+            }
+        }
+        /// <summary>
+        /// Paramless constructor for future EF use
+        /// </summary>
+        public NewEmployee()
+        {
+            
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Visible;
+    }
+}
---
+++ WhiteHingePCL/Classes/NewEmployeeCollection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteHingePCL.Classes
+{
+    /// <summary>
+    /// A New Employee Collection
+    /// </summary>
+    public class NewEmployeeCollection : List<NewEmployee>
+    {
+        /// <summary>
+        /// For finding user by their UserId
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <returns></returns>
+        public NewEmployee FindUserById(int employeeId)
+        {
+            return this.Single(x => x.PayrollId == employeeId);
+        }
+        /// <summary>
+        /// For finding a user based on their active directory name. Case-insensitive
+        /// </summary>
+        /// <param name="directoryUser"></param>
+        /// <returns></returns>
+        public NewEmployee FindUserByActiveDirectoryName(string directoryUser)
+        {
+            return this.Single(x => string.Equals(x.AdUser, directoryUser, StringComparison.CurrentCultureIgnoreCase));
+        }
+        /// <summary>
+        /// Sorts the Collection by Surname
+        /// </summary>
+        public void SortBySurname()
+        {
+            this.Sort((x,y) => string.Compare(x.Surname, y.Surname, StringComparison.Ordinal));
+        }
+        /// <summary>
+        /// Sorts the collection by Id
+        /// </summary>
+        public void SortById()
+        {
+            this.Sort((x,y) => x.PayrollId.CompareTo(y.PayrollId));
+        }
+        /// <summary>
+        /// Constructor for use with SelectDataDictionary
+        /// </summary>
+        /// <param name="sqlData"></param>
+        public NewEmployeeCollection(List<Dictionary<string, object>> sqlData)
+        {
+            foreach (var result in sqlData)
+            {
+                this.Add(new NewEmployee(result));
+            }
+        }
+    }
+}
