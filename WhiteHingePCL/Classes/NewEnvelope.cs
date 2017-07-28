4691d807a3aa14af776e4ff97c301696a330d140
Owen <Floul1@hotmail.co.uk>
2017-07-28 11:57:42 +0100

    Added the new envelope

---
+++ WhiteHingePCL/Classes/NewEnvelope.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WhiteHingePCL.Classes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NewEnvelope
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonConstructor]
+        public NewEnvelope()
+        {
+            
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string envCode;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string envName;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int envWeight;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string envSize;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string envFrom;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int envBoxQuan;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public float envBoxNet;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public float envIndCost;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool NeedsBox;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool isHidden;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string envNewName;
+    }
+}
